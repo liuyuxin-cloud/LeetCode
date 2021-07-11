88fbd6c7f366e88ca213181f86076b90c6738afe
刘昱欣 <2590177089@qq.com>
2021-07-11 22:33:59 +0800

    7.11

--- shuzu/src/July11.java
+++ shuzu/src/July11.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定一个无重复元素的有序整数数组 nums 。
+ *
+ * 返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表。也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个范围但不属于 nums 的数字 x 。
+ *
+ * 列表中的每个区间范围 [a,b] 应该按如下格式输出：
+ *
+ * "a->b" ，如果 a != b
+ * "a" ，如果 a == b
+ */
 public class July11 {
     public static void main(String[] args) {
         Solution7_11 s = new Solution7_11();
