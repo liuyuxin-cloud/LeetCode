20deefdb41504d8a5fb76694292a23d586b4b020
刘昱欣 <2590177089@qq.com>
2021-10-21 20:53:19 +0800

    10.21

--- leetcode/src/October20_1.java
+++ leetcode/src/October20_1.java
@@ -3,6 +3,10 @@
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  */
 public class October20_1 {
+    public static void main(String[] args){
+        int[] nums = new int[]{4,3,-5,2};
+        System.out.println(new Solution10_20_1().maxSubArray(nums));
+    }
 }
 
 class Solution10_20_1 {
---
+++ leetcode/src/October21.java
@@ -0,0 +1,30 @@
+/**
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ */
+
+public class October21 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, -1, 4};
+        System.out.println(new Solution10_21().maxProduct(nums));
+    }
+
+}
+
+class Solution10_21 {
+    public int maxProduct(int[] nums) {
+        int length = nums.length;
+        int[] maxF = new int[length];
+        int[] minF = new int[length];
+        System.arraycopy(nums, 0, maxF, 0, length);
+        System.arraycopy(nums, 0, minF, 0, length);
+        for (int i = 1; i < length; ++i) {
+            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
+            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+        }
+        int ans = maxF[0];
+        for (int i = 1; i < length; ++i) {
+            ans = Math.max(ans, maxF[i]);
+        }
+        return ans;
+    }
+}
