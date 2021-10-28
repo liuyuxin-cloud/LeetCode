3ca8bf5b7ba065f3ee6cc7c244074737459c4fa0
刘昱欣 <2590177089@qq.com>
2021-10-28 19:43:40 +0800

    10.28

--- leetcode/src/October27.java
+++ leetcode/src/October27.java
@@ -5,10 +5,11 @@
  */
 
 public class October27 {
-    public static void main(String[] args){
-        System.out.println(new Solution10_27().uniquePaths(10,10));
+    public static void main(String[] args) {
+        System.out.println(new Solution10_27().uniquePaths(10, 10));
     }
 }
+
 class Solution10_27 {
     public int uniquePaths(int m, int n) {
         long ans = 1;
@@ -17,12 +18,22 @@ class Solution10_27 {
         }
         return (int) ans;
     }
+
     //动态规划
     public int uniquePaths1(int m, int n) {
-        long ans = 1;
-        for (int x = n, y = 1; y < m; ++x, ++y) {
-            ans = ans * x / y;
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
         }
-        return (int) ans;
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
     }
+
 }
