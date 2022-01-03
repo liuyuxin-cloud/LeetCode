4baa7a24361cfa3603298af16aabb3511d81fea2
刘昱欣 <2590177089@qq.com>
2022-01-03 21:35:17 +0800

    3

---
+++ leetcode/src/twenty22/January3.java
@@ -0,0 +1,35 @@
+package twenty22;
+
+public class January3 {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution1_22 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
