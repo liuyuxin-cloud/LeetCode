13751bc7603b0b4d391f44959421488c86005c62
刘昱欣 <2590177089@qq.com>
2022-04-12 22:29:32 +0800

    12

---
+++ leetcode/src/twenty22/April12.java
@@ -0,0 +1,19 @@
+package twenty22;
+
+import java.util.Arrays;
+
+public class April12 {
+}
+class Solution4_22 {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n + 1];
+        nums[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+        }
+        return Arrays.stream(nums).max().getAsInt();
+    }
+}
