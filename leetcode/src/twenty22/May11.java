facc96485205f8f180aa104757c7d38d086267f2
刘昱欣 <2590177089@qq.com>
2022-05-11 19:32:56 +0800

    5.11

---
+++ leetcode/src/twenty22/May11.java
@@ -0,0 +1,16 @@
+package twenty22;
+
+import java.util.Arrays;
+
+public class May11 {
+}
+ class Solution5_11 {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int num : nums) {
+            sum += Math.abs(nums[nums.length / 2] - num);
+        }
+        return sum;
+    }
+}
