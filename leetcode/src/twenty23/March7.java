fe0aa79ea93dad0633683d73c20f6a223c376d7f
刘昱欣 <2590177089@qq.com>
2023-03-08 14:46:21 +0800

    2

--- leetcode/src/twenty23/March7.java
+++ leetcode/src/twenty23/March7.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class March7 {
     public static void main(String[] args) {
         Solution3_7 s = new Solution3_7();
-        System.out.println(s.PredictTheWinner(new int[]{1,5,2}));
+        System.out.println(s.PredictTheWinner1(new int[]{1,5,2}));
     }
 
 }
@@ -15,12 +15,27 @@ class Solution3_7 {
         return total(nums, 0, nums.length - 1, 1) >= 0;
     }
 
-    public int total (int[] nums, int l, int r, int turn) {
+    public int total(int[] nums, int l, int r, int turn) {
         if (l == r) {
             return nums[l] * turn; // 递归最里层
         }
-        int scoreStart = nums[l] * turn + total(nums, l+1, r, -turn); // 选择第一个递归下一次
-        int scoreEnd = nums[r] * turn + total(nums, l, r-1, -turn);
+        int scoreStart = nums[l] * turn + total(nums, l + 1, r, -turn); // 选择第一个递归下一次
+        int scoreEnd = nums[r] * turn + total(nums, l, r - 1, -turn);
         return Math.max(scoreStart * turn, scoreEnd * turn) * turn; // 递归最外层返回结果
+    }
+
+    // dp[i][j] = Max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
+    public boolean PredictTheWinner1(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for(int i = nums.length-1; i >= 0; i--) {
+            for(int j = i+1; j < nums.length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+        return dp[0][nums.length-1] > 0;
     }
 }
