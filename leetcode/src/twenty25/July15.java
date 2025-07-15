25e8ef25ef8403c66f1b7a3cc070073ebe05ea93
刘昱欣 <2590177089@qq.com>
2025-07-15 17:54:44 +0800

    7.15

---
+++ leetcode/src/twenty25/July15.java
@@ -0,0 +1,27 @@
+package twenty25;
+
+//56. 合并区间
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class July15 {
+}
+
+class Solution7_15 {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[0][2];
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
