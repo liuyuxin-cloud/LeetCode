0adde65f27a33266c18ed60f566a38acb45dfaa2
刘昱欣 <2590177089@qq.com>
2025-09-18 22:38:44 +0800

    9.18

---
+++ leetcode/src/twenty25/September18.java
@@ -0,0 +1,25 @@
+package twenty25;
+
+//
+
+public class September18 {
+}
+
+class Solution9_18 {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
+    }
+}
