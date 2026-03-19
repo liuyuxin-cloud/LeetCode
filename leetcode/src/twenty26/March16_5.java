fefb70b353c5912df9bc9ca70bc217b68a644ae9
刘昱欣 <2590177089@qq.com>
2026-03-20 00:02:07 +0800

    3.19

--- leetcode/src/twenty26/March16_5.java
+++ leetcode/src/twenty26/March16_5.java
@@ -1,53 +1,69 @@
 package twenty26;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+
+
+//127. 单词接龙
 
 public class March16_5 {
 }
 
 class Solution3_16_5 {
-    static class Pair {
-        String cur;
-        int num;
-        public Pair(String cur, int num) {
-            this.cur = cur;
-            this.num = num;
-        }
-    }
+    Map<String, Integer> wordId = new HashMap<String, Integer>();
+    List<List<Integer>> edge = new ArrayList<List<Integer>>();
+    int nodeNum = 0;
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (!wordList.contains(endWord)) {
+        for (String word: wordList) {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.containsKey(endWord)) {
             return 0;
         }
-        if (beginWord.equals(endWord)) {
-            return 1;
-        }
-        boolean[] visited = new boolean[wordList.size()];
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(beginWord, 1));
-        while (!queue.isEmpty()) {
-            Pair curWord = queue.poll();
-            for (String str: wordList) {
-                if (!visited[wordList.indexOf(str)] && isNext(curWord.cur, str)) {
-                    if (str.equals(endWord)) {
-                        return curWord.num + 1;
-                    }
-                    queue.offer(new Pair(str, curWord.num + 1));
-                    visited[wordList.indexOf(str)] = true;
+        int[] dis = new int[nodeNum]; // 到所有节点所需步骤
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+        dis[beginId] = 0;
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        que.offer(beginId);
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            if (x == endId) {
+                return dis[endId] / 2 + 1;
+            }
+            for (int it: edge.get(x)) { // 从开始的单词开始广度搜索，只走之前没经过的节点
+                if (dis[it] == Integer.MAX_VALUE) {
+                    dis[it] = dis[x] + 1;
+                    que.offer(it);
                 }
             }
         }
         return 0;
     }
 
-    private boolean isNext(String cur, String next) {
-        int count = 0;
-        for (int i = 0; i < cur.length(); i++) {
-            if (cur.charAt(i) != next.charAt(i)) {
-                count++;
-            }
+    public void addEdge(String word) { // 每个单词连接所有通配符替换的新单词
+        addWord(word);
+        int id1 = wordId.get(word);
+        char[] array = word.toCharArray();
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            char tmp = array[i];
+            array[i] = '*';
+            String newWord = new String(array); // 换上通配符
+            addWord(newWord);
+            int id2 = wordId.get(newWord);
+            edge.get(id1).add(id2);
+            edge.get(id2).add(id1);
+            array[i] = tmp;
         }
-        return count == 1;
+    }
+
+    public void addWord(String word) {
+        if (!wordId.containsKey(word)) {
+            wordId.put(word, nodeNum++); //存单词及对应的id
+            edge.add(new ArrayList<Integer>()); //单词节点对应的边=相邻节点
+        }
     }
 }
---
+++ leetcode/src/twenty26/March19.java
@@ -0,0 +1,13 @@
+package twenty26;
+
+import java.util.List;
+
+//373. 查找和最小的 K 对数字
+public class March19 {
+}
+
+class Solution3_19 {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+
+    }
+}
